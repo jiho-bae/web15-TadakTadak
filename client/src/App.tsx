e0f3fef1868c951f15af5f0beed89a3f7e9ded7b
jiho-bae <jiho@dgu.ac.kr>
2022-08-02 22:59:50 +0900

    Feat: Suspense + Lazy load 적용 및 실험

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
 import './styles/fonts.css';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import ToastContainer from '@components/toast/ToastContainer';
 import { useUser, useUserFns } from '@contexts/userContext';
 import { getUserByToken } from '@src/apis';
 import Introduction from '@pages/Introduction';
-import Main from '@pages/Main';
-import Tadak from '@pages/Tadak';
-import CampFire from '@pages/Campfire';
-import Profile from '@pages/Profile';
+
+const Main = lazy(() => import('@pages/Main'));
+const Tadak = lazy(() => import('@pages/Tadak'));
+const CampFire = lazy(() => import('@pages/Campfire'));
+const Profile = lazy(() => import('@pages/Profile'));
 
 const App = (): JSX.Element => {
   const user = useUser();
@@ -32,14 +33,16 @@ const App = (): JSX.Element => {
     <>
       <ToastContainer />
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Introduction} />
-          <Route path="/main" component={Main} />
-          <Route path="/room/tadak" component={Tadak} />
-          <Route path="/room/campfire" component={CampFire} />
-          <Route path="/profile" component={Profile} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Introduction} />
+            <Route path="/main" component={Main} />
+            <Route path="/room/tadak" component={Tadak} />
+            <Route path="/room/campfire" component={CampFire} />
+            <Route path="/profile" component={Profile} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
       <GlobalStyles />
     </>
